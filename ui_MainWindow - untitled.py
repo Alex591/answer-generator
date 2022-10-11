6a2fd79124a39ca0e993a1845d29d88b87f8b1fd
Alex Adam <alex.adam@ni.com>
2022-10-11 22:47:37 +0200

    ui

--- ui_MainWindow - untitled.py
+++ ui_MainWindow - untitled.py
@@ -18,130 +18,97 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QLabel,
     QLineEdit, QMainWindow, QPushButton, QSizePolicy,
     QStatusBar, QWidget)
+import styledark_rc
 
 class Ui_WindowsAnswerfile(object):
     def setupUi(self, WindowsAnswerfile):
         if not WindowsAnswerfile.objectName():
             WindowsAnswerfile.setObjectName(u"WindowsAnswerfile")
-        WindowsAnswerfile.resize(503, 528)
+        WindowsAnswerfile.resize(478, 578)
+        WindowsAnswerfile.setMinimumSize(QSize(478, 578))
+        WindowsAnswerfile.setMaximumSize(QSize(478, 578))
         palette = QPalette()
-        brush = QBrush(QColor(0, 0, 0, 255))
-        brush.setStyle(Qt.SolidPattern)
-        palette.setBrush(QPalette.Active, QPalette.WindowText, brush)
-        brush1 = QBrush(QColor(240, 240, 240, 255))
-        brush1.setStyle(Qt.SolidPattern)
-        palette.setBrush(QPalette.Active, QPalette.Button, brush1)
-        brush2 = QBrush(QColor(255, 255, 255, 255))
-        brush2.setStyle(Qt.SolidPattern)
-        palette.setBrush(QPalette.Active, QPalette.Light, brush2)
-        brush3 = QBrush(QColor(227, 227, 227, 255))
-        brush3.setStyle(Qt.SolidPattern)
-        palette.setBrush(QPalette.Active, QPalette.Midlight, brush3)
-        brush4 = QBrush(QColor(160, 160, 160, 255))
-        brush4.setStyle(Qt.SolidPattern)
-        palette.setBrush(QPalette.Active, QPalette.Dark, brush4)
-        palette.setBrush(QPalette.Active, QPalette.Mid, brush4)
-        palette.setBrush(QPalette.Active, QPalette.Text, brush)
-        palette.setBrush(QPalette.Active, QPalette.BrightText, brush2)
-        palette.setBrush(QPalette.Active, QPalette.ButtonText, brush)
-        palette.setBrush(QPalette.Active, QPalette.Base, brush2)
-        palette.setBrush(QPalette.Active, QPalette.Window, brush1)
-        brush5 = QBrush(QColor(105, 105, 105, 255))
-        brush5.setStyle(Qt.SolidPattern)
-        palette.setBrush(QPalette.Active, QPalette.Shadow, brush5)
-        brush6 = QBrush(QColor(0, 120, 215, 255))
-        brush6.setStyle(Qt.SolidPattern)
-        palette.setBrush(QPalette.Active, QPalette.Highlight, brush6)
-        palette.setBrush(QPalette.Active, QPalette.HighlightedText, brush2)
-        brush7 = QBrush(QColor(0, 0, 255, 255))
-        brush7.setStyle(Qt.SolidPattern)
-        palette.setBrush(QPalette.Active, QPalette.Link, brush7)
-        brush8 = QBrush(QColor(255, 0, 255, 255))
-        brush8.setStyle(Qt.SolidPattern)
-        palette.setBrush(QPalette.Active, QPalette.LinkVisited, brush8)
-        brush9 = QBrush(QColor(245, 245, 245, 255))
-        brush9.setStyle(Qt.SolidPattern)
-        palette.setBrush(QPalette.Active, QPalette.AlternateBase, brush9)
-        brush10 = QBrush(QColor(0, 0, 0, 255))
-        brush10.setStyle(Qt.NoBrush)
-        palette.setBrush(QPalette.Active, QPalette.NoRole, brush10)
-        brush11 = QBrush(QColor(255, 255, 220, 255))
-        brush11.setStyle(Qt.SolidPattern)
-        palette.setBrush(QPalette.Active, QPalette.ToolTipBase, brush11)
-        palette.setBrush(QPalette.Active, QPalette.ToolTipText, brush)
-        palette.setBrush(QPalette.Inactive, QPalette.WindowText, brush)
-        palette.setBrush(QPalette.Inactive, QPalette.Button, brush1)
-        palette.setBrush(QPalette.Inactive, QPalette.Light, brush2)
-        palette.setBrush(QPalette.Inactive, QPalette.Midlight, brush3)
-        palette.setBrush(QPalette.Inactive, QPalette.Dark, brush4)
-        palette.setBrush(QPalette.Inactive, QPalette.Mid, brush4)
-        palette.setBrush(QPalette.Inactive, QPalette.Text, brush)
-        palette.setBrush(QPalette.Inactive, QPalette.BrightText, brush2)
-        palette.setBrush(QPalette.Inactive, QPalette.ButtonText, brush)
-        palette.setBrush(QPalette.Inactive, QPalette.Base, brush2)
-        palette.setBrush(QPalette.Inactive, QPalette.Window, brush1)
-        palette.setBrush(QPalette.Inactive, QPalette.Shadow, brush5)
-        palette.setBrush(QPalette.Inactive, QPalette.Highlight, brush1)
-        palette.setBrush(QPalette.Inactive, QPalette.HighlightedText, brush)
-        palette.setBrush(QPalette.Inactive, QPalette.Link, brush7)
-        palette.setBrush(QPalette.Inactive, QPalette.LinkVisited, brush8)
-        palette.setBrush(QPalette.Inactive, QPalette.AlternateBase, brush9)
-        brush12 = QBrush(QColor(0, 0, 0, 255))
-        brush12.setStyle(Qt.NoBrush)
-        palette.setBrush(QPalette.Inactive, QPalette.NoRole, brush12)
-        palette.setBrush(QPalette.Inactive, QPalette.ToolTipBase, brush11)
-        palette.setBrush(QPalette.Inactive, QPalette.ToolTipText, brush)
-        brush13 = QBrush(QColor(120, 120, 120, 255))
-        brush13.setStyle(Qt.SolidPattern)
-        palette.setBrush(QPalette.Disabled, QPalette.WindowText, brush13)
-        palette.setBrush(QPalette.Disabled, QPalette.Button, brush1)
-        palette.setBrush(QPalette.Disabled, QPalette.Light, brush2)
-        brush14 = QBrush(QColor(247, 247, 247, 255))
-        brush14.setStyle(Qt.SolidPattern)
-        palette.setBrush(QPalette.Disabled, QPalette.Midlight, brush14)
-        palette.setBrush(QPalette.Disabled, QPalette.Dark, brush4)
-        palette.setBrush(QPalette.Disabled, QPalette.Mid, brush4)
-        palette.setBrush(QPalette.Disabled, QPalette.Text, brush13)
-        palette.setBrush(QPalette.Disabled, QPalette.BrightText, brush2)
-        palette.setBrush(QPalette.Disabled, QPalette.ButtonText, brush13)
-        palette.setBrush(QPalette.Disabled, QPalette.Base, brush1)
-        palette.setBrush(QPalette.Disabled, QPalette.Window, brush1)
-        palette.setBrush(QPalette.Disabled, QPalette.Shadow, brush)
-        palette.setBrush(QPalette.Disabled, QPalette.Highlight, brush6)
-        palette.setBrush(QPalette.Disabled, QPalette.HighlightedText, brush2)
-        palette.setBrush(QPalette.Disabled, QPalette.Link, brush7)
-        palette.setBrush(QPalette.Disabled, QPalette.LinkVisited, brush8)
-        palette.setBrush(QPalette.Disabled, QPalette.AlternateBase, brush9)
-        brush15 = QBrush(QColor(0, 0, 0, 255))
-        brush15.setStyle(Qt.NoBrush)
-        palette.setBrush(QPalette.Disabled, QPalette.NoRole, brush15)
-        palette.setBrush(QPalette.Disabled, QPalette.ToolTipBase, brush11)
-        palette.setBrush(QPalette.Disabled, QPalette.ToolTipText, brush)
         WindowsAnswerfile.setPalette(palette)
+        font = QFont()
+        WindowsAnswerfile.setFont(font)
         WindowsAnswerfile.setFocusPolicy(Qt.ClickFocus)
         WindowsAnswerfile.setToolTipDuration(3)
+        WindowsAnswerfile.setStyleSheet(u"")
         self.centralwidget = QWidget(WindowsAnswerfile)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.centralwidget.setStyleSheet(u"background-color: rgb(243, 243, 243);")
         self.pushButton = QPushButton(self.centralwidget)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(160, 430, 181, 61))
-        self.pushButton.setStyleSheet(u"")
-        self.user_label = QLabel(self.centralwidget)
-        self.user_label.setObjectName(u"user_label")
-        self.user_label.setGeometry(QRect(50, 370, 71, 21))
+        self.pushButton.setGeometry(QRect(150, 430, 181, 61))
+        font1 = QFont()
+        font1.setFamilies([u"Segoe UI Variable"])
+        font1.setUnderline(False)
+        font1.setStrikeOut(False)
+        self.pushButton.setFont(font1)
+        self.pushButton.setStyleSheet(u".QPushButton {\n"
+"	background-color:#0069ba;\n"
+"	border-radius:9px;\n"
+"	border:1px solid #ffffff;\n"
+"	display:inline-block;\n"
+"	cursor:pointer;\n"
+"	color:#ffffff;\n"
+"	font-family:Segoe UI Variable;\n"
+"	font-size:21px;\n"
+"	padding:16px 31px;\n"
+"	text-decoration:none;\n"
+"}\n"
+".QPushButton:hover {\n"
+"	background-color:#1a79c1;\n"
+"}\n"
+".QPushButton:active {\n"
+"	position:relative;\n"
+"	top:1px;\n"
+"}\n"
+"")
         self.user_edit_button = QPushButton(self.centralwidget)
         self.user_edit_button.setObjectName(u"user_edit_button")
-        self.user_edit_button.setGeometry(QRect(110, 370, 75, 24))
-        self.hdd_edit_button = QPushButton(self.centralwidget)
-        self.hdd_edit_button.setObjectName(u"hdd_edit_button")
-        self.hdd_edit_button.setEnabled(False)
-        self.hdd_edit_button.setGeometry(QRect(280, 290, 131, 24))
-        self.user_label_3 = QLabel(self.centralwidget)
-        self.user_label_3.setObjectName(u"user_label_3")
-        self.user_label_3.setGeometry(QRect(50, 330, 71, 21))
+        self.user_edit_button.setGeometry(QRect(250, 350, 181, 31))
+        self.user_edit_button.setStyleSheet(u".QPushButton {\n"
+"	background-color:#ffffff;\n"
+"	border-radius:4px;\n"
+"	border:1px solid #8a8886;\n"
+"	display:inline-block;\n"
+"	cursor:pointer;\n"
+"	color:#565555;\n"
+"	font-family:Segoe UI Variable;\n"
+"	font-weight:bold;\n"
+"	font-size:13px;\n"
+"	text-decoration:none;\n"
+"}\n"
+".QPushButton:hover {\n"
+"	background-color:#f3f2f1;\n"
+"}\n"
+".QPushButton:active {\n"
+"	position:relative;\n"
+"	top:1px;\n"
+"}")
         self.program_edit_button = QPushButton(self.centralwidget)
         self.program_edit_button.setObjectName(u"program_edit_button")
-        self.program_edit_button.setGeometry(QRect(110, 330, 75, 24))
+        self.program_edit_button.setGeometry(QRect(40, 350, 181, 31))
+        self.program_edit_button.setStyleSheet(u".QPushButton {\n"
+"	background-color:#ffffff;\n"
+"	border-radius:4px;\n"
+"	border:1px solid #8a8886;\n"
+"	display:inline-block;\n"
+"	cursor:pointer;\n"
+"	color:#565555;\n"
+"	font-family:Segoe UI Variable;\n"
+"	font-weight:bold;\n"
+"	font-size:13px;\n"
+"	text-decoration:none;\n"
+"}\n"
+".QPushButton:hover {\n"
+"	background-color:#f3f2f1;\n"
+"}\n"
+".QPushButton:active {\n"
+"	position:relative;\n"
+"	top:1px;\n"
+"}")
         self.vm_checkBox = QCheckBox(self.centralwidget)
         self.vm_checkBox.setObjectName(u"vm_checkBox")
         self.vm_checkBox.setGeometry(QRect(170, 80, 151, 20))
@@ -151,26 +118,79 @@ class Ui_WindowsAnswerfile(object):
 #endif // QT_CONFIG(whatsthis)
         self.vm_checkBox.setTristate(False)
         self.win_edition_combo = QComboBox(self.centralwidget)
+        self.win_edition_combo.addItem("")
+        self.win_edition_combo.addItem("")
         self.win_edition_combo.setObjectName(u"win_edition_combo")
         self.win_edition_combo.setGeometry(QRect(170, 50, 231, 22))
+        font2 = QFont()
+        font2.setFamilies([u"Segoe UI Variable Display Semib"])
+        font2.setBold(True)
+        self.win_edition_combo.setFont(font2)
+        self.win_edition_combo.setStyleSheet(u"/* Main ComboBox Style */\n"
+"#win_edition_combo {\n"
+"border: 1px solid #d3d3d3;\n"
+"border-radius: 4px;\n"
+"padding-left: 10px;\n"
+"background-color: rgb(250, 250, 250);\n"
+"}\n"
+"/*Dropdown area */\n"
+"#win_edition_combo::drop-down{\n"
+"	border:0px\n"
+"}\n"
+"#win_edition_combo::down-arrow{\n"
+"image: url(:/ComboBox/img dark/ComboBoxDisabled.png)\n"
+"\n"
+"}\n"
+"\n"
+"#win_edition_combo::on{\n"
+"border: 2px solid #c2dbfe\n"
+"}\n"
+"\n"
+"")
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
         self.label.setGeometry(QRect(50, 50, 111, 16))
+        self.label.setFont(font2)
         self.hdd_checkbox = QCheckBox(self.centralwidget)
         self.hdd_checkbox.setObjectName(u"hdd_checkbox")
-        self.hdd_checkbox.setGeometry(QRect(280, 260, 211, 20))
+        self.hdd_checkbox.setGeometry(QRect(270, 260, 211, 20))
+        self.hdd_checkbox.setFont(font2)
         self.label_2 = QLabel(self.centralwidget)
         self.label_2.setObjectName(u"label_2")
         self.label_2.setGeometry(QRect(50, 120, 111, 16))
+        self.label_2.setFont(font2)
         self.label_3 = QLabel(self.centralwidget)
         self.label_3.setObjectName(u"label_3")
         self.label_3.setGeometry(QRect(50, 80, 91, 16))
+        self.label_3.setFont(font2)
         self.win_language_combobox = QComboBox(self.centralwidget)
         self.win_language_combobox.setObjectName(u"win_language_combobox")
         self.win_language_combobox.setGeometry(QRect(170, 120, 231, 22))
+        self.win_language_combobox.setStyleSheet(u"/* Main ComboBox Style */\n"
+"#win_language_combobox {\n"
+"border: 1px solid #d3d3d3;\n"
+"border-radius: 4px;\n"
+"padding-left: 10px;\n"
+"background-color: rgb(250, 250, 250);\n"
+"}\n"
+"/*Dropdown area */\n"
+"#win_language_combobox::drop-down{\n"
+"	border:0px\n"
+"}\n"
+"#win_language_combobox::down-arrow{\n"
+"image: url(:/ComboBox/img dark/ComboBoxDisabled.png)\n"
+"\n"
+"}\n"
+"\n"
+"#win_language_combobox::on{\n"
+"border: 2px solid #c2dbfe\n"
+"}\n"
+"\n"
+"")
         self.label_4 = QLabel(self.centralwidget)
         self.label_4.setObjectName(u"label_4")
         self.label_4.setGeometry(QRect(50, 260, 101, 16))
+        self.label_4.setFont(font2)
         self.uac_checkbox = QCheckBox(self.centralwidget)
         self.uac_checkbox.setObjectName(u"uac_checkbox")
         self.uac_checkbox.setGeometry(QRect(180, 260, 31, 20))
@@ -182,6 +202,7 @@ class Ui_WindowsAnswerfile(object):
         self.label_5 = QLabel(self.centralwidget)
         self.label_5.setObjectName(u"label_5")
         self.label_5.setGeometry(QRect(50, 290, 121, 16))
+        self.label_5.setFont(font2)
         self.codeintegrity_checkbox = QCheckBox(self.centralwidget)
         self.codeintegrity_checkbox.setObjectName(u"codeintegrity_checkbox")
         self.codeintegrity_checkbox.setGeometry(QRect(180, 290, 31, 20))
@@ -196,12 +217,14 @@ class Ui_WindowsAnswerfile(object):
         self.label_6 = QLabel(self.centralwidget)
         self.label_6.setObjectName(u"label_6")
         self.label_6.setGeometry(QRect(50, 160, 101, 16))
+        self.label_6.setFont(font2)
         self.lineEdit_2 = QLineEdit(self.centralwidget)
         self.lineEdit_2.setObjectName(u"lineEdit_2")
         self.lineEdit_2.setGeometry(QRect(170, 200, 231, 22))
         self.label_7 = QLabel(self.centralwidget)
         self.label_7.setObjectName(u"label_7")
         self.label_7.setGeometry(QRect(50, 200, 111, 16))
+        self.label_7.setFont(font2)
         WindowsAnswerfile.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(WindowsAnswerfile)
         self.statusbar.setObjectName(u"statusbar")
@@ -221,11 +244,8 @@ class Ui_WindowsAnswerfile(object):
         WindowsAnswerfile.setStatusTip("")
 #endif // QT_CONFIG(statustip)
         self.pushButton.setText(QCoreApplication.translate("WindowsAnswerfile", u"Generate", None))
-        self.user_label.setText(QCoreApplication.translate("WindowsAnswerfile", u"Users", None))
-        self.user_edit_button.setText(QCoreApplication.translate("WindowsAnswerfile", u"Edit", None))
-        self.hdd_edit_button.setText(QCoreApplication.translate("WindowsAnswerfile", u"Edit HDD Partitions", None))
-        self.user_label_3.setText(QCoreApplication.translate("WindowsAnswerfile", u"Programs", None))
-        self.program_edit_button.setText(QCoreApplication.translate("WindowsAnswerfile", u"Edit", None))
+        self.user_edit_button.setText(QCoreApplication.translate("WindowsAnswerfile", u"Edit Users", None))
+        self.program_edit_button.setText(QCoreApplication.translate("WindowsAnswerfile", u"Edit Preinstalled Programs", None))
 #if QT_CONFIG(tooltip)
         self.vm_checkBox.setToolTip("")
 #endif // QT_CONFIG(tooltip)
@@ -233,8 +253,12 @@ class Ui_WindowsAnswerfile(object):
         self.vm_checkBox.setStatusTip(QCoreApplication.translate("WindowsAnswerfile", u"[For Windows 11] Adds the bypasses necessary to install it in a virtual machine", None))
 #endif // QT_CONFIG(statustip)
         self.vm_checkBox.setText("")
+        self.win_edition_combo.setItemText(0, QCoreApplication.translate("WindowsAnswerfile", u"Windows 10", None))
+        self.win_edition_combo.setItemText(1, QCoreApplication.translate("WindowsAnswerfile", u"Windows 11", None))
+
+        self.win_edition_combo.setProperty("placeholderText", QCoreApplication.translate("WindowsAnswerfile", u"Windows 11 Pro", None))
         self.label.setText(QCoreApplication.translate("WindowsAnswerfile", u"Windows Edition", None))
-        self.hdd_checkbox.setText(QCoreApplication.translate("WindowsAnswerfile", u"Manual HDD partition assignment", None))
+        self.hdd_checkbox.setText(QCoreApplication.translate("WindowsAnswerfile", u"Automatic HDD assignment", None))
         self.label_2.setText(QCoreApplication.translate("WindowsAnswerfile", u"Windows Language", None))
         self.label_3.setText(QCoreApplication.translate("WindowsAnswerfile", u"Virtual Machine", None))
         self.label_4.setText(QCoreApplication.translate("WindowsAnswerfile", u"Enable UAC", None))
